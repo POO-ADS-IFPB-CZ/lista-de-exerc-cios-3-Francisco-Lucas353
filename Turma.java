2468bed4f212bf614e7b64a2e2758803ee0e9dd8
Francisco-Lucas353 <FrancisLucasSM@gmail.com>
2025-05-17 23:04:27 -0300

    Questão 1

---
+++ Turma.java
@@ -0,0 +1,36 @@
+package Questão1;
+
+import java.util.ArrayList;
+
+public class Turma {
+    private ArrayList<Aluno> alunos;
+
+    public Turma() {
+        this.alunos = new ArrayList<>();
+    }
+
+    public void adicionarAluno(Aluno aluno) {
+        alunos.add(aluno);
+    }
+
+    public ArrayList<Aluno> listarAprovados() {
+        ArrayList<Aluno> aprovados = new ArrayList<>();
+        for (Aluno aluno : alunos) {
+            if (aluno.estaAprovado()) {
+                aprovados.add(aluno);
+            }
+        }
+        return aprovados;
+    }
+
+    public ArrayList<Aluno> listarReprovados() {
+        ArrayList<Aluno> reprovados = new ArrayList<>();
+        for (Aluno aluno : alunos) {
+            if (!aluno.estaAprovado()) {
+                reprovados.add(aluno);
+            }
+        }
+        return reprovados;
+    }
+}
+
